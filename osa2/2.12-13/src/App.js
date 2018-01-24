308dad988a8b94db7b1dd36c0dc7f60ef1f92a18
Topi Laanti <laantop@gmail.com>
2018-01-24 17:28:28 +0200

    Osa2

--- osa2/2.12-13/src/App.js
+++ osa2/2.12-13/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios'
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -7,6 +10,16 @@ class App extends React.Component {
       countries: [],
       filter: ''
     }
+  }
+
+  componentWillMount() {
+    console.log('will mount')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        this.setState({ countries: response.data })
+      })
   }
 
   handleFilterChange = (event) => {
@@ -21,19 +34,71 @@ class App extends React.Component {
           state={this.state}
           handleFilterChange={this.handleFilterChange}
         />
+        <CountriesToShow
+          countries={this.state.countries}
+          filter={this.state.filter}
+        />
       </div>
     );
   }
 }
 
-let Kentta = (props) => {
+const Kentta = (props) => {
   return (
-    <form>
-      find countries: <input
-        value={props.state.filter}
-        onChange={props.handleFilterChange}
-      />
-    </form>
+    <div>
+      <form>
+        find countries: <input
+          value={props.state.filter}
+          onChange={props.handleFilterChange}
+        />
+      </form>
+    </div>
+  )
+}
+
+const CountriesToShow = ({ countries, filter }) => {
+
+  const countriesToShow =
+    countries.filter(country => country.name.toUpperCase().includes(filter.toUpperCase()))
+
+  if (countriesToShow.length === 1) {
+    return (
+      <Country country={countriesToShow[0]} />
+    )
+  } else if (countriesToShow.length > 10) {
+    return (
+      <div>
+        <p>too many matches, specify another filter</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <ul>
+          {countriesToShow.map(country =>
+            <CountryList key={country.name} country={country} />)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h3>{country.name} </h3>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <img src={country.flag} height="100" width="120" alt="lippu" />
+    </div>
+  )
+}
+
+const CountryList = ({ country }) => {
+  return (
+    <div onClick={console.log('jee')}>
+      {country.name}
+    </div>
   )
 }
 
